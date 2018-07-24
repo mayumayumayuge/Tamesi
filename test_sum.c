a9896213c04c9d7c1920be31df37e69d700912b3
mayumayumayuge <33414337+mayumayumayuge@users.noreply.github.com>
2018-07-24 17:45:58 +0900

    fromPC3_branch1_1

--- test_sum.c
+++ test_sum.c
@@ -5,15 +5,17 @@ int main(){
     double x = 0;
     double y = 0;
     double z = 0;
+    double w = 0;
     double sum = 0;
 
     x = 3.3;
     y = 7.14;
     z = 7.1;
+    w = 7.04;
 
-    sum = x + y - z;
+    sum = x + y - z * w;
 
-    printf("x = %lf, y = %lf, z = %lf,  x + y - z = %lf\n", x, y, z, sum);
+    printf("x = %lf, y = %lf, z = %lf, w = %lf, x + y - z * w= %lf\n", x, y, z, w, sum);
 
     return 0;
 }
