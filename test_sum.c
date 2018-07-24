194455963435b3fe360b1a68236192d70f99f5ed
mayumayumayuge <33414337+mayumayumayuge@users.noreply.github.com>
2018-07-24 17:10:25 +0900

    fromPC1

---
+++ test_sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    double x = 0;
+    double y = 0;
+    double sum = 0;
+
+    x = 7.1;
+    y = 7.14;
+
+    sum = x + y;
+
+    printf("x = %lf, y = %lf, x + y = %lf\n", x, y, sum);
+
+    return 0;
+}
